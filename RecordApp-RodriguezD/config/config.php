5cd8b328b377d8d8032921246f9436b8c06c34a1
dianarod28 <201880207@psu.palawan.edu.ph>
2022-04-11 14:10:40 +0800

    Finish

---
+++ RecordApp-RodriguezD/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+    define('ROOT_URL','index.php');
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','simple');
+    define('DB_NAME','recordsapp');
---
+++ RecordApp-RodriguezD/config/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if(mysqli_connect_errno()){
+    echo 'failed to connect to MySQL'.mysqli_connect_errno();
+}
